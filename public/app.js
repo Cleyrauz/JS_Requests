5c23ce11f8d9e75e5b2a19fcd8cb3fe1e5736ecf
Cleyra Uzcategui <cleyra.uzcategui@gmail.com>
2018-08-01 18:14:39 +0100

    Added app function, requestComplete and makeRequest to app.js

--- public/app.js
+++ public/app.js
@@ -1,5 +1,21 @@
-var app = function(){
+const makeRequest = function(url, callback){
+  const request = new XMLHttpRequest();
+  request.open('GET', url);
+  request.addEventListener('load', callback);
+  request.send();
+}
 
+const requestComplete = function(){
+  if(this.status !== 200) return;
+  const jsonString = this.responseText;
+  const beers = JSON.parse(jsonString);
+  populateList(beers);
+}
+
+
+var app = function(){
+const url = 'https://api.punkapi.com/v2/beers';
+makeRequest(url, requestComplete);
 }
 
 window.addEventListener('load', app);
